a67c9dd38f8c06873e740ac6418bc6b240969ef7
otodtt <dtenevoto@mail.bg>
2022-09-23 17:20:57 +0300

    23.09.22 sl

---
+++ resources/views/quality/certificates/test.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.quality')
+@section('title')
+    {{ 'Всички Фирми' }}
+@endsection
+
+@section('css')
+    {!!Html::style("css/firms_objects/firms_all_css.css" )!!}
+    {!!Html::style("css/table/jquery.dataTables.css" )!!}
+    {!!Html::style("css/table/table_firms.css " )!!}
+@endsection
+
+@section('message')
+    @include('layouts.alerts.success')
+@endsection
+
+@section('content')
+    <div class="div-layout-title" style="margin-bottom: 20px; margin-top: 20px">
+        <h4 class="bold layout-title">TEST</h4>
+    </div>
+
+    {{--@include('quality.importers.table')--}}
+@endsection
+
+@section('scripts')
+    {!!Html::script("js/table/jquery-1.11.3.min.js" )!!}
+    {!!Html::script("js/table/jquery.dataTables.js" )!!}
+    {!!Html::script("js/table/firmsImportersTable.js" )!!}
+@endsection
